c4f1ccc7c9ec2c8d3b6777f7f6f885431d31ed69
Arunmey <arunmey@gmail.com>
2015-01-31 12:43:28 -0600

    EpicU First Challenge
    01/31/2015 sample

---
+++ MyFirstChallenge1/MyFirstChallenge1/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string money = pocketMoneyTextBox.Text;
+
+            resultLabel.Text = "At " + age + " years old, I would have expected you to have more than $" + money + " in your pocket.";
+
+
+        }
+    }
+}
---
+++ MyFirstChallenge1/MyFirstChallenge1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstChallenge1.Global" Language="C#" %>
